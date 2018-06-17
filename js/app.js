c9089817427c2a20e7b789efe6ba186cca1bcc0f
kshravi86 <kshravi86@gmail.com>
2018-06-17 09:57:54 +0530

    bv don't j

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.d
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/', {templateUrl: 'partials/homeView.html', controller: 'HomeCtrl'});
         $routeProvider.when('/view1', {templateUrl: 'partials/intro.html'});
-        $routeProvider.when('/view2', {templateUrl: 'partials/linear_regression.html'});
+        $routeProvider.when('/view2', {templateUrl: 'partials/seqmodels.html'});
         $routeProvider.when('/view3', {templateUrl: 'partials/helloworld.html'});
         $routeProvider.when('/view4', {templateUrl: 'partials/basic_operations.html'});
         $routeProvider.when('/view5', {templateUrl: 'partials/logistic_regression.html'});
