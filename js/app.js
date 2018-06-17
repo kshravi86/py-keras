d24e3a44ce326fc29fbb835b006954e820966971
kshravi86 <kshravi86@gmail.com>
2018-06-17 22:15:35 +0530

    vvv

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.d
         $routeProvider.when('/', {templateUrl: 'partials/homeView.html', controller: 'HomeCtrl'});
         $routeProvider.when('/view1', {templateUrl: 'partials/intro.html'});
         $routeProvider.when('/view2', {templateUrl: 'partials/seqmodels.html'});
-        $routeProvider.when('/view3', {templateUrl: 'partials/helloworld.html'});
+        $routeProvider.when('/view3', {templateUrl: 'partials/imdb_cnn.html'});
         $routeProvider.when('/view4', {templateUrl: 'partials/basic_operations.html'});
         $routeProvider.when('/view5', {templateUrl: 'partials/logistic_regression.html'});
         $routeProvider.when('/view6', {templateUrl: 'partials/BasicTensorFlow.html'});
